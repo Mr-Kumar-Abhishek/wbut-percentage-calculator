62683fa5e2d0e83b9b36f209a409484f7b87e192
Abhishek Kumar <mr.kumar.abhishek@email.com>
2019-09-29 08:42:04 +0000

    🍖⛪️ Checkpoint ./index.html:197979/534 ./script.js:197979/303

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 /* If you're feeling fancy you can add interactivity 
     to your site with Javascript */
 
-// prints "hi" in the browser's dev tools console
-console.log('hi');
+// checks if js is running or not
+console.log('JS is running');
+
+function empty_check() {
+  
+}
+
+function calculate() {
+  console.log("calculate is running ..");
+  $("#submit").click()
+  empty_check();
+}
+
+$(calculate());
